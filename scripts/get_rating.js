0090cfa02b673f78b8b8f1eb2e7b8e820100a890
Stephen Day <57306301+StephenDay1@users.noreply.github.com>
2026-03-11 20:54:13 -0600

    I like CSS
    I'd say it's about two hours of work up to this point

--- scripts/get_rating.js
+++ scripts/get_rating.js
@@ -3,7 +3,21 @@ function get_rating(elements) {
     elements.forEach(element => {
         professorName = element.textContent;
         if (professorName != "TBD") {
-            console.log(professorName);
+            
+            // This would eventually be the actual query
+            
+            score = professorName[0].toLowerCase() < 'f' ? 5 : professorName[0].toLowerCase() < 'q' ? 3.5 : 1.5;
+            console.log(professorName + ": " + score);
+
+            // Make it look pretty!
+            color = score > 4 ? "#7ff6c3" : score > 3 ? "#fff170" : "#ff9c9c";
+
+            element.style.backgroundColor = "#f7f7f7";
+            element.style.borderRadius = "4px";
+            element.style.padding = "4px 1px 4px 4px";
+            element.style.borderLeft = `5px solid ${color}`;
+            element.style.cursor = "pointer";
+            
         }
     });
 }
@@ -11,6 +25,18 @@ function get_rating(elements) {
 console.log("Rate This BYU Professor is active!");
 // Wait 5 seconds.
 setTimeout(() => {
-    get_rating(document.querySelectorAll(".sectionDetailsCol > h4"));
+    const fragment = window.location.hash.substring(1);
+    console.log(typeof(fragment));
+    if (fragment == "/") {
+        // Home page
+        console.log("Getting scores for your professors");
+        get_rating(document.querySelectorAll(".cdSectionRoot > :nth-child(3)"));
+    } else if (fragment.includes("chooseASection")) {
+        // In class selection
+        console.log("Getting scores for available professors");
+        get_rating(document.querySelectorAll(".sectionDetailsCol > h4"));
+    }
+
+
 }, 5000);
 
